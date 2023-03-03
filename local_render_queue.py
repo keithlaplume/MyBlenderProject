263548130612956fe1a094fb2762541b61bb3540
Keith <contact@keithlaplume.com>
2023-03-03 09:08:34 -0500

    MOVE TO NEXT JOB AFTER BLENDER QUIT

--- local_render_queue.py
+++ local_render_queue.py
@@ -1,13 +1,7 @@
 import subprocess
 
-RENDER_QUEUE = [r"J:\UNTITLED_MARS_PROJECT\assets\materials\working_files\mars_sand_smallrocks_albedo.blend",
-                r"J:\UNTITLED_MARS_PROJECT\assets\materials\working_files\mars_sand_smallrocks_roughness.blend",
-                r"J:\UNTITLED_MARS_PROJECT\assets\materials\working_files\mars_sand_allrocks_albedo.blend",
-                r"J:\UNTITLED_MARS_PROJECT\assets\materials\working_files\mars_sand_allrocks_roughness.blend",
-                r"J:\UNTITLED_MARS_PROJECT\assets\materials\working_files\mars_sand_bigrocks_albedo.blend",
-                r"J:\UNTITLED_MARS_PROJECT\assets\materials\working_files\mars_sand_bigrocks_roughness.blend",
-                r"J:\UNTITLED_MARS_PROJECT\assets\materials\working_files\mars_sand_pebbles_albedo.blend",
-                r"J:\UNTITLED_MARS_PROJECT\assets\materials\working_files\mars_sand_pebbles_roughness.blend"]
+RENDER_QUEUE = [r"J:\UNTITLED_MARS_PROJECT\assets\materials\working_files\mars_sand_allrocks_albedo.blend",
+                r"J:\UNTITLED_MARS_PROJECT\assets\materials\working_files\mars_sand_allrocks_roughness.blend"]
 
 blender_path = '"C:/Program Files/Blender Foundation/Blender 3.4/blender.exe"'
 
@@ -17,7 +11,7 @@ for blender_file in RENDER_QUEUE:
     # print output
     while True:
         output = p.stdout.readline()
-        if output == '' and p.poll() is not None:
+        if 'Blender quit' in str(output.strip()):
             break
         if output:
             print(output.strip())
