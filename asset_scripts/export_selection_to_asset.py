8340bea533e38bed15731eac9c332e58d03baf19
Keith LaPlume <contact@keithlaplume.com>
2024-06-22 16:57:06 -0400

    use full strength emission when creating other bakes

--- asset_scripts/export_selection_to_asset.py
+++ asset_scripts/export_selection_to_asset.py
@@ -11,9 +11,9 @@ def pre_process_transparent(node_tree):
     if transparent:
         print("Found transparent")
         metalness_value = node_tree.nodes.new("ShaderNodeValue")
-        metalness_value.outputs[0].default_value = 0.5
+        metalness_value.outputs[0].default_value = 0.7
         alpha_value = node_tree.nodes.new("ShaderNodeValue")
-        alpha_value.outputs[0].default_value = 0.6
+        alpha_value.outputs[0].default_value = 0.7
         emit_color = node_tree.nodes.new("ShaderNodeRGB")
         emit_color.outputs[0].default_value = (1, 0.928203, 0.333327, 1)
         emit_strength = node_tree.nodes.new("ShaderNodeValue")
@@ -68,27 +68,29 @@ def bake_out_asset_maps(selection, bake_list, asset_name, publish_path, final_si
     bpy.context.scene.render.bake.use_pass_direct = False
     bpy.context.scene.render.bake.use_pass_indirect = False
     bpy.context.scene.render.bake.margin = 1
-    bpy.context.scene.render.bake.use_clear = False
 
     size = final_size * oversample
 
+    # disconnect transparent bdsf
+    prepared_materials = []
+    for ob in selection:
+        for material in ob.material_slots:
+            if material.name not in prepared_materials:
+                node_tree = material.material.node_tree
+                pre_process_transparent(node_tree)
+                prepared_materials.append(material.name)
+
+    non_mesh_list = []
     for bake_type in bake_list:
         new_image = bpy.data.images.new("bake_" + bake_type, width=size, height=size)
         if bake_type == "alpha":
             new_image.generated_color = (1, 1, 1, 1)
+            bpy.context.scene.render.bake.use_clear = False
+        else:
+            bpy.context.scene.render.bake.use_clear = True
         path = os.path.join(publish_path, "4k", bake_type + ".png")
         img_format = 'PNG'
         emit_nodes = {}
-        prepared_materials = []
-        non_obj_list = []
-
-        # disconnect transparent bdsf
-        for ob in selection:
-            for material in ob.material_slots:
-                if material.name not in prepared_materials:
-                    node_tree = material.material.node_tree
-                    pre_process_transparent(node_tree)
-                    prepared_materials.append(material.name)
 
         prepared_materials = []
         for ob in selection:
@@ -118,8 +120,13 @@ def bake_out_asset_maps(selection, bake_list, asset_name, publish_path, final_si
                             except:
                                 emit_node = nodes.new("ShaderNodeRGB")
                                 emit_node.outputs[0].default_value = bsdf.inputs[26].default_value
+                            try:
+                                strength_node = bsdf.inputs[27].links[0].from_socket.node
+                            except:
+                                strength_node = nodes.new("ShaderNodeValue")
+                                strength_node.outputs[0].default_value = bsdf.inputs[27].default_value
 
-                            emit_nodes[material.name] = emit_node
+                            emit_nodes[material.name] = [emit_node, strength_node]
 
                             try:
                                 metallic_node = bsdf.inputs[1].links[0].from_socket.node
@@ -129,6 +136,9 @@ def bake_out_asset_maps(selection, bake_list, asset_name, publish_path, final_si
 
                             # connect metallic node to emit input
                             node_tree.links.new(metallic_node.outputs[0], bsdf.inputs[26])
+                            bake_strength_node = nodes.new("ShaderNodeValue")
+                            bake_strength_node.outputs[0].default_value = 1.0
+                            node_tree.links.new(bake_strength_node.outputs[0], bsdf.inputs[27])
 
                         # bake out alpha through the emission
                         # connect alpha to emit
@@ -140,8 +150,13 @@ def bake_out_asset_maps(selection, bake_list, asset_name, publish_path, final_si
                             except:
                                 emit_node = nodes.new("ShaderNodeRGB")
                                 emit_node.outputs[0].default_value = bsdf.inputs[26].default_value
+                            try:
+                                strength_node = bsdf.inputs[27].links[0].from_socket.node
+                            except:
+                                strength_node = nodes.new("ShaderNodeValue")
+                                strength_node.outputs[0].default_value = bsdf.inputs[27].default_value
 
-                            emit_nodes[material.name] = emit_node
+                            emit_nodes[material.name] = [emit_node, strength_node]
 
                             try:
                                 alpha_node = bsdf.inputs[4].links[0].from_socket.node
@@ -151,11 +166,14 @@ def bake_out_asset_maps(selection, bake_list, asset_name, publish_path, final_si
 
                             # connect alpha node to emit input
                             node_tree.links.new(alpha_node.outputs[0], bsdf.inputs[26])
+                            bake_strength_node = nodes.new("ShaderNodeValue")
+                            bake_strength_node.outputs[0].default_value = 1.0
+                            node_tree.links.new(bake_strength_node.outputs[0], bsdf.inputs[27])
 
                         prepared_materials.append(material.name)
 
             else:
-                non_obj_list.append(ob)
+                non_mesh_list.append(ob)
                 ob.select_set(False)
 
         bake_and_save_image(new_image, path, img_format, bake_type, size, final_size)
@@ -170,10 +188,11 @@ def bake_out_asset_maps(selection, bake_list, asset_name, publish_path, final_si
                         emit_node = emit_nodes[material.name]
                         node_tree = material.material.node_tree
                         bsdf = node_tree.nodes["Principled BSDF"]
-                        node_tree.links.new(emit_node.outputs[0], bsdf.inputs[26])
+                        node_tree.links.new(emit_node[0].outputs[0], bsdf.inputs[26])
+                        node_tree.links.new(emit_node[1].outputs[0], bsdf.inputs[27])
 
     #add non objects back into selection
-    for ob in non_obj_list:
+    for ob in non_mesh_list:
         ob.select_set(True)
 
     #reset render settings
@@ -186,7 +205,7 @@ def bake_out_asset_maps(selection, bake_list, asset_name, publish_path, final_si
 
 
 def create_material_from_folder(folder, bake_list):
-    new_mat = bpy.data.materials.new(os.path.basename(folder))
+    new_mat = bpy.data.materials.new(os.path.dirname(folder))
     new_mat.use_nodes = True
     mat_nodes = new_mat.node_tree.nodes
     bsdf = mat_nodes["Principled BSDF"]
