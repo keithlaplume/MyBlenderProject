16b5e7f5097441a47bf9403ca66c57fadd19544b
Keith LaPlume <contact@keithlaplume.com>
2024-07-26 10:10:31 -0400

    make fisrt selection object active object

--- asset_scripts/batch_original_to_proxy1.py
+++ asset_scripts/batch_original_to_proxy1.py
@@ -17,6 +17,7 @@ bake_list = ["Diffuse", "Metallic", "Emit", "Roughness", "Normal"]
 
 bpy.ops.object.select_all(action='SELECT')
 selection = bpy.context.selected_objects
+bpy.context.view_layer.objects.active = selection[0]
 asset_name = os.path.basename(bpy.data.filepath).split(".blend")[0].replace("_original", "_proxy1")
 publish_path = os.path.dirname(bpy.data.filepath)
 
